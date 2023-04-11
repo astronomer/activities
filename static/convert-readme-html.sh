1536f6caf1b6f843889d111bc8a7a1978bf3b33d
faizan-qazi <112110852+faizan-qazi@users.noreply.github.com>
2023-04-11 22:27:48 +0530

    Create convert-readme-html.sh

---
+++ static/convert-readme-html.sh
@@ -0,0 +1,8 @@
+echo "Generating Pseudo HTML Code"
+marked -i README.md -o pseudo.html
+echo "Generating HTML Formatted Document"
+python3 ./static/format-pseudo-html.py
+rm pseudo.html
+echo "Generating Styles"
+sed -i '' 's/<pre><code>/<pre><code class="language-py">/g' index.html
+echo "Done"
